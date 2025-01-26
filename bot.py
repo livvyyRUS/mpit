7566d6f5571479fe4b1bd6caa9991c7f8e615533
livvyy <94890179+livvyyRUS@users.noreply.github.com>
2025-01-26 10:16:56 +0300

    asd
    asd

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 from config import token
 
@@ -10,10 +11,16 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=token)
 dp = Dispatcher()
 
+url = f"https://192.168.52.16:2222"
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    btn = InlineKeyboardButton(text="Открыть мини-приложение",
+                               web_app=WebAppInfo(url=f"{url}/login/{message.from_user.id}/euaugfg87aegf")
+                               )
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[btn]])
+    await message.answer("Hello!1", reply_markup=keyboard)
 
 
 async def main():
