028663876891d1d600fc1e9be6888b815c63f03b
livvyy <94890179+livvyyRUS@users.noreply.github.com>
2025-01-26 09:24:05 +0300

    commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+from config import token
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
